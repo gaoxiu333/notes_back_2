ba69c64d6383ebf64674d0bd870ca32ee1f48f53
xiu <gao.yh1991@gmail.com>
2023-02-21 23:18:11 +0800

    一个简单的算法题

---
+++ 算法/算法联系/简单联系.js
@@ -0,0 +1,11 @@
+// 给定一个正整数数组和一个目标值，请你在该数组中找出两个相加等于目标值的两个正整数，并返回他们的下标。
+function findSumTarget(nums,target){
+    const {length} = nums
+    const dirrs = {}
+    for(let i;i<length;i++){
+        if(dirrs[target-nums[i]] !== undefined) return [dirrs[target - num[i]],i]
+        dirrs[nums[i]] = i
+    }
+}
+
+// 双指针法
